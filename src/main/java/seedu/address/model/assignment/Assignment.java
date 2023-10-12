2de6ecd99dd3ac2b87f2089ed71e1d0ed62206ce
Yuan Jing <zcyuanjing@gmail.com>
2023-10-12 14:06:13 +0800

    Feat: Add Assignment Model

---
+++ src/main/java/seedu/address/model/assignment/Assignment.java
@@ -0,0 +1,116 @@
+package seedu.address.model.assignment;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.tag.Tag;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Assignment {
+
+    // Identity fields
+    private final Name name;
+
+    // Data fields
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private final Set<Tag> tags = new HashSet<>();
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Assignment(Name name, LocalDateTime start, LocalDateTime end, Set<Tag> tags) {
+        requireAllNonNull(name, start, end, tags);
+        this.name = name;
+        this.start = start;
+        this.end = end;
+        this.tags.addAll(tags);
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+
+    /**
+     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
+     * if modification is attempted.
+     */
+    public Set<Tag> getTags() {
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSamePerson(Assignment otherAssignment) {
+        if (otherAssignment == this) {
+            return true;
+        }
+
+        return otherAssignment != null
+                && otherAssignment.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Assignment)) {
+            return false;
+        }
+
+        Assignment otherAssignment = (Assignment) other;
+        return name.equals(otherAssignment.name)
+                && this.start.equals(otherAssignment.start)
+                && this.end.equals(otherAssignment.end)
+                && tags.equals(otherAssignment.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, start, end, tags);
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter toStringFormatter = DateTimeFormatter.ofPattern("d MMM uuuu h:mm a");
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("start", start.format(toStringFormatter))
+                .add("end", end.format(toStringFormatter))
+                .add("tags", tags)
+                .toString();
+    }
+
+}
