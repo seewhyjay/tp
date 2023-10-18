3161d867c8fe03be0ee0bbe4692896b788595462
teeheehoo\jierongu <jiarongchoo@gmail.com>
2023-10-18 11:03:06 +0800

    Fix lexicographical order for import statements

--- src/main/java/seedu/address/logic/parser/assignment/EditAssignmentParser.java
+++ src/main/java/seedu/address/logic/parser/assignment/EditAssignmentParser.java
@@ -9,9 +9,9 @@ import static seedu.address.logic.parser.assignment.CliSyntax.PREFIX_NAME;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.assignment.EditAssignmentCommand;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
-import seedu.address.logic.commands.assignment.EditAssignmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.person.Prefix;
 import seedu.address.model.assignment.Description;
@@ -31,7 +31,8 @@ public class EditAssignmentParser {
         ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args, PREFIX_INDEX, PREFIX_DESCRIPTION);
         if (!arePrefixesPresent(argumentMultimap, PREFIX_NAME, PREFIX_ENDDATE)
                 || !argumentMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditAssignmentCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    EditAssignmentCommand.MESSAGE_USAGE));
         }
         Index index = ParserUtil.parseIndex(argumentMultimap.getValue(PREFIX_INDEX).get());
         Description newDescription = ParserUtil.parseDescription(
