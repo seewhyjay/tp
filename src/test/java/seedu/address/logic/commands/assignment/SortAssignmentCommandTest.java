014c20f61d40fefb6c8d8b99555eb0c58c38ba5f
Chow Yuan Jing <zcyuanjing@gmail.com>
2023-10-20 11:55:45 +0800

    Chore: Lint

--- src/test/java/seedu/address/logic/commands/assignment/SortAssignmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/assignment/SortAssignmentCommandTest.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalAssignments.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
